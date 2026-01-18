a19575dbdd1ae553edb2ee3ae266994ddd47273f
fares2.718 <faresobaid2715@gmail.com>
2026-01-18 20:32:32 +0400

    Added .gitignore

--- CRMD-Server/CRMD/CRMD.Api/Program.cs
+++ CRMD-Server/CRMD/CRMD.Api/Program.cs
@@ -12,6 +12,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular",
+        policy =>
+        {
+            policy
+                .WithOrigins("http://localhost:4200")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
 var connectionString =
     builder.Configuration.GetConnectionString("Postgres")!;
 
@@ -32,6 +44,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAngular");
 
 app.UseAuthorization();
 
