4f257096fa5ee9531105517a3a6ecdc6c6e64bf0
fares2.718 <faresobaid2715@gmail.com>
2026-01-19 14:43:17 +0400

    created client side project

--- CRMD-Server/CRMD/CRMD.Domain/obj/Debug/net8.0/CRMD.Domain.AssemblyInfo.cs
+++ CRMD-Server/CRMD/CRMD.Domain/obj/Debug/net8.0/CRMD.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRMD.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+415c916419ef4a0b5d1c6705e373ce1fed81d22e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5afb0bedcb75c9978c5b410734cecf8d35c6d9ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRMD.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRMD.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
